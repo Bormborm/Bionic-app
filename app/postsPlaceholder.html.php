6d06435fdce4bf47ac6c2e9fb58e0387e937741b
Bormborm <extra-ultra@meta.ua>
2016-07-19 17:44:40 +0300

    add templates for output

---
+++ app/postsPlaceholder.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="posts-section">
+    <?php foreach ($response as $postsArray){
+             foreach ($postsArray as $text){
+             print_r("<div>$text</div>")
+
+</div>
+
+</body>
+</html>
