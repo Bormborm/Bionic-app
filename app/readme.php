26140a724679bc916040c761101832ee01a4c521
Bormborm <extra-ultra@meta.ua>
2016-08-23 02:00:00 +0300

    comment and post adding and deleting.

--- app/readme.php
+++ app/readme.php
@@ -1,7 +1,8 @@
 <?php
 
 //TODO: templates' parents and inherits
-//TODO: || authentication and validation
+//TODO: || authentication and validation: LoggedIn check (+cookies), registration and login errors on the same page.
+//TODO: ability to post only on self page.
 //TODO: ||| images and files
-//TODO: || info view without page reload
+//TODO: | info view without page reload - partially done without ajax
 
