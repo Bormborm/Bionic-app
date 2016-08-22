b8b8214529e83ac900e1e4a594e81ce56454bf1e
Bormborm <extra-ultra@meta.ua>
2016-08-23 01:59:48 +0300

    comment and post adding and deleting.

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -9,9 +9,6 @@ use Bormborm\Services\ValidationService;
 
 class MainController extends TemplateController
 {
-
-    private $pr;
-
     /**
      * @return string
      */
@@ -33,7 +30,6 @@ class MainController extends TemplateController
         {
             $this->getUserAction($_SESSION['id']);
         }
-
         return $render;
     }
 
@@ -46,7 +42,7 @@ class MainController extends TemplateController
     {
         $val = new ValidationService();
 
-        $id = ($userId) ? $userId : (int) $_GET['id'];
+        $id = ($userId) ? $userId : $_GET['id'];
         if ($val->checkUserExists((int) $id)) {
 
             $userRep = new User();
@@ -55,7 +51,7 @@ class MainController extends TemplateController
                 'user.twig',
                 [
                     'user' => $_SESSION['name'],
-                    'id' => $_SESSION['id'],//$user->getId(),
+                    'id' => $user->getId(),
                     'name' => $user->getName(),
                     'lastname' => $user->getLastname(),
                     'posts' => $user->getPosts()
@@ -65,22 +61,12 @@ class MainController extends TemplateController
         return $user;
     }
 
-    public function getPostAction()
-    {
-        $post = new Post();
-        if (!($_GET['id']) == null) {
-            $response = $post->getAllByUserId((int) $_GET['id']);
-        } else {
-            $response = $post->getAllByUserId(1);
-        }
-        return $response;
-    }
-
     public function addPostAction()
     {
         $pr = new Post();
         if (!empty($_POST['postText'])) {
             $pr->addNewPost($_POST['postText'], $_SESSION['id']);
+            unset($_POST);
         }
         $this->getUserAction();
     }
@@ -107,18 +93,41 @@ class MainController extends TemplateController
 
     public function addCommentAction()
     {
+        $cr = new Comment();
         if (!empty($_POST['addedComment']) && (!empty($_POST['postId']))) {
-            $postId = (int)$_POST['postId'];
-            $commentRepository->addNewCommentByUserId($_SESSION['id'], $_POST['addedComment'], $postId);
+            $cr->addNewCommentByUserId($_SESSION['id'], $_POST['addedComment'], (int) $_POST['postId']);
+            unset($_POST);
         }
+        $this->getUserAction($_GET['id']);
     }
 
     public function deleteCommentAction()
     {
+        $cr = new Comment();
         if (!empty($_POST['deleteComment'])) {
-            $id = (int)$_POST['deleteComment'];
-            $commentRepository->deleteComment($id);
+
+            $cr->deleteComment((int) $_POST['deleteComment']);
         }
+        $this->getUserAction($_GET['id']);
+    }
+
+    // Not used.
+    public function getPostAction()
+    {
+        $post = new Post();
+        if (!($_GET['id']) == null) {
+            $response = $post->getAllByUserId((int) $_GET['id']);
+        } else {
+            $response = $post->getAllByUserId(1);
+        }
+        $this->templater->render(
+            'homepage.twig',
+            [
+                'user' => $_SESSION['name'],
+                'posts' => $response
+            ]
+        );
+        return $response;
     }
 
 }
