ce69519ba21c681edce9483bf40864f157170124
c114 <c114@bionic.local>
2016-08-23 20:44:11 +0300

    readme

--- app/readme.php
+++ app/readme.php
@@ -1,9 +1,8 @@
 <?php
 
 //TODO: templates' parents and inherits
-//TODO: || authentication and validation: LoggedIn check (+cookies), registration and login errors on the same page.
+//TODO: || authentication and validation: LoggedIn check (+cookies), registration and login errors on the same page.  isauthor/isallowed
 //TODO: ability to post only on self page.
 //TODO: ||| images and files
 //TODO: | info view without page reload - partially done without ajax
 
-// xhttpauthtoken
